e245bee508b77bffe31b41b036bda5a7cd4454b9
Xiaobei <1963239048@qq.com>
2025-07-02 15:20:16 +0800

    初始化项目

---
+++ src/main/java/edu/jit/weather_analysis/WeatherAnalysisApplication.java
@@ -0,0 +1,13 @@
+package edu.jit.weather_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAnalysisApplication.class, args);
+    }
+
+}
