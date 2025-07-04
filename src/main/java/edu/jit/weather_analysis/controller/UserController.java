69881c0c297023a4073a87b86e3384268a81df0f
Xiaobei <1963239048@qq.com>
2025-07-04 08:59:12 +0800

    实现session保存登录信息和登出功能

--- src/main/java/edu/jit/weather_analysis/controller/UserController.java
+++ src/main/java/edu/jit/weather_analysis/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 /**
  * 用户控制器
@@ -37,14 +38,24 @@ public class UserController {
     }
 
     @PostMapping(value = {"login"})
-    public String login(String username, String password) {
+    public String login(String username, String password, HttpSession session) {
         // 实现登录
         User user = userRepository.login(username, password);
         // 登录成功，页面重定向
         if (user != null) {
+            // 存储用户信息
+            session.setAttribute("user", user);
             return "redirect:/index";
         }
-        // 登录失败，页面重定向
+        // 登录失败, 移除用户信息
+        session.removeAttribute("user");
+        // 重新登录
+        return "redirect:/login";
+    }
+
+    @GetMapping(value = {"logout"})
+    public String logout(HttpSession session) {
+        session.removeAttribute("user");
         return "redirect:/login";
     }
 }
